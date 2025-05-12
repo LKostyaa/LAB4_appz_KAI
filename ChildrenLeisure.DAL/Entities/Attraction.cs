3072d46260aa62c1bea8ad941a0ea2f9e883eb32
Kostiantyn Lobach <klobach06@gmail.com>
2025-05-12 17:05:01 +0300

    Додав бібліотеки класів, налаштував ChildrenLeisure.DAL, додав базову структуру класів до ChildrenLeisure.BLL

---
+++ ChildrenLeisure.DAL/Entities/Attraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildrenLeisure.DAL.Entities
+{
+    public class Attraction
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price {  get; set; }
+        public int? EntertainmentZoneId { get; set; }
+        public virtual EntertainmentZone EntertainmentZone { get; set; }
+    }
+}
